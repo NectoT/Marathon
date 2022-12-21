b10220143014afe8d27e580d79f419f4cbd18c59
NectoT <44334753+NectoT@users.noreply.github.com>
2022-12-21 22:54:55 +0300

    Чернышов_1
    Это был его код, но я его немного поломал и заново коммичу, представим что это был он и теперь очередь Паши

--- task0.py
+++ task0.py
@@ -5,3 +5,13 @@
 # Напишите максимально правдоподобную физику воды и верните обновлённый массив terrain после симуляции физики воды на протияжении n секунд 
 # (То есть обновлённый terrain по прошествию n секунд).
 # Реалистичность алгоритма будет оценена по десятибальной шкале человеком, не участвовавшим в написании кода.
+def main(terrain:list[list[int]], g: int, n: int) -> list[list[int]]:
+    for i in range(len(terrain)):
+        current_level = terrain[i]
+        for pixel in range(len(current_level)):
+            if current_level[pixel] == 2:
+                buff = i + 1
+                while True:
+                    if terrain[buff][pixel] == 0:
+                        current_level[pixel], terrain[buff][pixel] = 0, 2
+                    else: break
